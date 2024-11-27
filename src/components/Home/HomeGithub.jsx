1f94e140a30caf2d9df7e97609a3869015bcaec1
WickyPlays <nguyenthienbao8601@gmail.com>
2024-11-27 07:54:32 +0700

    Update

--- src/components/Home/HomeGithub.jsx
+++ src/components/Home/HomeGithub.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import './HomeTitle.scss';
+import './HomeGithub.scss';
 
-export default function HomeTitle() {
+export default function HomeGithub() {
 
   return (
     <div className={`home-github`}>
-     <p>Watch my github</p>
+      <div className='content'>
+        <div>
+          <p className='subtitle'>Join our link at</p>
+          <p className='title'>https://hcm202-product-g9.github.io</p>
+        </div>
+        <div className='github'>
+          <p className='subtitle'>Source code</p>
+          <p className='title'>https://github.com/WickyPlays/hcm202-product-g9</p>
+        </div>
+
+      </div>
 
     </div>
   );
