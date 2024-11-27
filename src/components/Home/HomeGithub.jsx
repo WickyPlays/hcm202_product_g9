18af23c16d7e387ec84e8a8527cc80bebc4b66e4
WickyPlays <nguyenthienbao8601@gmail.com>
2024-11-27 09:10:14 +0700

    Update

--- src/components/Home/HomeGithub.jsx
+++ src/components/Home/HomeGithub.jsx
@@ -7,12 +7,12 @@ export default function HomeGithub() {
     <div className={`home-github`}>
       <div className='content'>
         <div>
-          <p className='subtitle'>Join our link at</p>
-          <p className='title'>https://wickyplays.github.io/hcm202_product_g9</p>
+          <p className='subtitle'>Tham gia link tại</p>
+          <a className='title' href='https://wickyplays.github.io/hcm202_product_g9'>https://wickyplays.github.io/hcm202_product_g9</a>
         </div>
         <div className='github'>
-          <p className='subtitle'>Source code</p>
-          <p className='title'>https://github.com/WickyPlays/hcm202_product_g9</p>
+          <p className='subtitle'>Mã nguồn code</p>
+          <a className='title' href='https://github.com/WickyPlays/hcm202_product_g9'>https://github.com/WickyPlays/hcm202_product_g9</a>
         </div>
 
       </div>
--- src/components/Home/HomeMembers.jsx
+++ src/components/Home/HomeMembers.jsx
@@ -1,7 +1,57 @@
+import React from 'react';
+import { Box, Typography, Stack } from '@mui/material';
+import './HomeMembers.scss';
+
+const timelineData = [
+  { member: 'Member 1', profession: 'Engineer' },
+  { member: 'Member 2', profession: 'Designer' },
+  { member: 'Member 3', profession: 'Developer' },
+  { member: 'Member 4', profession: 'Manager' },
+];
+
 export default function HomeMembers() {
   return (
     <div className='home-members'>
-      <p>Members</p>
+      <div className='contents'>
+        <p className='title'>Thành viên nhóm 9</p>
+        <div>
+          <div className='node'>
+            <div className='circle'>
+
+            </div>
+            <p className='name'>Lâm Quốc Vinh</p>
+            <div className='line'></div>
+            <p className='role'>Nhóm trưởng</p>
+          </div>
+        </div>
+        <div className='group'>
+          <div className='node'>
+            <div className='circle'>
+
+            </div>
+            <p className='name'>Từ Thiên Bảo</p>
+            <div className='line'></div>
+            <p className='role'>Thiết kế sản phẩm</p>
+          </div>
+          <div className='node'>
+            <div className='circle'>
+
+            </div>
+            <p className='name'>Nguyễn Ngọc Tường Vy</p>
+            <div className='line'></div>
+            <p className='role'>Thuyết trình</p>
+          </div>
+          <div className='node'>
+            <div className='circle'>
+
+            </div>
+            <p className='name'>Thân Thành Tâm</p>
+            <div className='line'></div>
+            <p className='role'>Biên soạn nội dung</p>
+          </div>
+        </div>
+      </div>
+
     </div>
-  )
+  );
 }
